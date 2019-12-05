1374a21e0c7d91f70b0fbed32fdae1699f05733e
Dan Amyot <amyot.dan@gmail.com>
2019-12-05 00:56:21 -0500

    Question 2

--- src/Data.js
+++ src/Data.js
@@ -1,9 +1,9 @@
 let initialItems = [
   {
+    id: "asewq",
     description: "Nice boats. 50% off. wow.",
     price: 10000,
     image: "/boat.png",
-    id: "asewq",
     sellerId: "ewio"
   },
   {
@@ -12,6 +12,27 @@ let initialItems = [
     price: 50,
     image: "/lawnchair.jpg",
     sellerId: "xcvb"
+  },
+  {
+    id: "ewsdfa",
+    description: "Bowling Ball",
+    price: 110,
+    image: "/bowlingball.jpg",
+    sellerId: "xcvb"
+  },
+  {
+    id: "aodief",
+    description: "Car",
+    price: 25000,
+    image: "/car.jpg",
+    sellerId: "xkfr"
+  },
+  {
+    id: "widsao",
+    description: "Burger",
+    price: 5,
+    image: "/burger.jpg",
+    sellerId: "xkfr"
   }
 ];
 let initialSellers = [
@@ -24,6 +45,11 @@ let initialSellers = [
     id: "xcvb",
     name: "Hank Green",
     rating: "2 stars"
+  },
+  {
+    id: "xkfr",
+    name: "Sue Barnes",
+    rating: "4 stars"
   }
 ];
 export { initialItems, initialSellers };
